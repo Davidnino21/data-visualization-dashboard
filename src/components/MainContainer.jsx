848d6b82af46cc37da04f234602aff3b34a11123
Davidnino21 <davidnino_21@yahoo.com>
2025-02-27 12:49:54 -0800

    added line graph

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,18 +1,27 @@
+import LineGraph from "./LineGraph";
+
+
 function MainContainer() {
-    return(
-        <div className="bg-white flex-grow">
-            <h1 className="bg-slate-300 text-center font-bold p-10 text-4xl">Data Visualization Dashboard</h1>
-            <div className="p-6">
-            <label className="p-3 font-bold">Filter by Category:</label>
-            <select className="border-1" name="" id="">
-                <option value="">All</option>
-                <option value="">Technology</option>
-                <option value="">Finance</option>
-                <option value="">Healthcare</option>
-            </select>
-        </div>
-        </div>
-    )
+  return (
+    <div className="bg-white flex-grow">
+      <h1 className="bg-slate-300 text-center font-bold p-10 text-4xl">
+        Data Visualization Dashboard
+      </h1>
+      <div className="p-6">
+        <label className="p-3 font-bold">Filter by Category:</label>
+        <select className="border-1" name="" id="">
+          <option value="">All</option>
+          <option value="">Technology</option>
+          <option value="">Finance</option>
+          <option value="">Healthcare</option>
+        </select>
+      </div>
+      <div className="h-96 flex items-center flex-col">
+        <h2 className="text-center">Line Chart</h2>
+        <LineGraph />
+      </div>
+    </div>
+  );
 }
 
-export default MainContainer
+export default MainContainer;
